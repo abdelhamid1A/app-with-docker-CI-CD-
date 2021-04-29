d7be5f0bc9b14ccf9d51b7d6e6f06bbb1f767431
abdelhamid1A <hamoda.abdo@outlook.fr>
2021-04-29 10:52:23 +0000

    docker app

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const data = require('./data.json')
+
+app.use(express.urlencoded({extended : true}))
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.status(200).json(data)
+})
+app.get('/byId/:id',(req,res)=>{
+    const id = req.params.id
+    const user = data.find(item =>item.id === id)
+    res.status(200).send(user)
+})
+
+app.listen(3000,()=>console.log('server run'))
+module.exports = app
